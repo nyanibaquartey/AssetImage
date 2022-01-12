881783c83b858a26d6cd573e12fa17baba2a2ec2
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-12 11:58:15 +0000

    Comment out unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:assetimage/main.dart';
+//import 'package:assetimage/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
